129f678da3ff29a8026197759e46ad45bb1a0bed
Caryen Song <caryensong@googlemai.com>
2024-12-17 00:18:55 +0100

    add geItem localStorage for favoritesBooks

--- script/favorits.js
+++ script/favorits.js
@@ -3,15 +3,20 @@
 let favoritesBooks =[];
 
 function renderFavorits(){
-   let booksCardsRef = document.getElementById("booksCards");
-   booksCardsRef.innerHTML = "";
-  for(let t = 0; t < books.length; t++) {
-      if(books[t].liked === true){
-        favoritesBooks.push(t);
-      } else { 
+  const storedBooks = JSON.parse(localStorage.getItem('updateBookStore'));
+
+  if (storedBooks) {
+     let booksCardsRef = document.getElementById("booksCards");
+     booksCardsRef.innerHTML = "";
+  
+  for(let t = 0; t < storedBooks.length; t++) {
+      if(storedBooks[t].liked === true){
+        favoritesBooks.push(t);     
+       } else { 
         continue;
-      }
-     booksCardsRef.innerHTML += getbooksTemplate(t);
+      } 
+      booksCardsRef.innerHTML += getbooksTemplate(t);
+ }
    }
  }
 console.log(favoritesBooks);
